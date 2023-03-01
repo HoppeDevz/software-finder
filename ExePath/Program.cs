504f8d7f9a9a41e9be1a7b766d72229b69bf5e26
Gabriel Hoppe <gabrielh2c@hotmail.com>
2023-03-01 09:37:28 -0300

    first commit

---
+++ ExePath/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ExePath
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            /*
+
+            SoftwareFinder finder = new SoftwareFinder();
+            string processName = finder.GetSoftwareProcessNameByFriendlyName("dbeaver");
+
+            Console.WriteLine(processName);
+            Console.ReadLine();
+
+            return;
+
+            */
+
+            if (args.Length == 0) return;
+
+            SoftwareFinder finder = new SoftwareFinder();
+
+            switch (args[0])
+            {
+
+                case "Get-All-Installed-Softwares":
+                {
+
+                    List<string> softwares = finder.GetAllProgramsInstalled();
+
+                    foreach (string software in softwares) { 
+                        
+                        Console.WriteLine(software);
+                    }
+
+
+                    return;
+                }
+
+                case "Get-Process-Name-By-Friendly-Name":
+                {
+                    if (args.Length < 2) return;
+
+                    string processName = finder.GetSoftwareProcessNameByFriendlyName(args[1]);
+                    Console.Write(processName);
+
+                    return;
+                }
+
+                default: return;
+            }
+
+        }
+    }
+}
---
+++ ExePath/SoftwareFinder.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace ExePath
+{
+    internal class SoftwareFinder
+    {
+
+        // EXAMPLE: GetAllProgramsInstalled() -> List('Google Chrome', 'Battle.net', 'Steam')
+        public List<string> GetAllProgramsInstalled()
+        {
+
+            List<string> programs = new List<string>();
+
+            string path64 = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            string path32 = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+
+            using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(path64))
+            {
+
+                foreach (string subkey_name in key.GetSubKeyNames())
+                {
+
+                    using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                    {
+
+                        if (subkey.GetValue("DisplayName") != null)
+                        {
+
+                            programs.Add(subkey_name);
+                        }
+                    }
+                }
+            }
+
+            using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(path32))
+            {
+
+                foreach (string subkey_name in key.GetSubKeyNames())
+                {
+
+                    using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                    {
+
+                        if (subkey.GetValue("DisplayName") != null)
+                        {
+
+                            programs.Add(subkey_name);
+                        }
+                    }
+                }
+            }
+
+            return programs;
+        }
+
+        // EXAMPLE: GetSoftwareProcessNameByFriendlyName('Google Chrome') -> chrome.exe
+        public string GetSoftwareProcessNameByFriendlyName(string softwareName)
+        {
+
+            foreach (Process process in Process.GetProcesses())
+            {
+
+                try
+                {
+
+                    if (
+                        process.MainModule != null &&
+                        process.MainModule.FileVersionInfo != null &&
+                        process.MainModule.FileVersionInfo.FileDescription != null
+                    ) {
+
+                        if (process.MainModule.FileVersionInfo.FileDescription.Equals(softwareName, StringComparison.OrdinalIgnoreCase))
+                        {
+
+                            return process.ProcessName;
+                        }
+                        
+                        if (process.MainModule.FileVersionInfo.ProductName.ToLower().Contains(softwareName.ToLower()))
+                        {
+                            return process.ProcessName;
+                        }
+                    }
+
+                    if (process.ProcessName.ToLower().Contains(softwareName.ToLower()) || process.MainModule.ModuleName.ToLower().Contains(softwareName.ToLower()))
+                    {
+
+                        return process.ProcessName;
+                    }
+
+                } catch (Exception exception)
+                {
+
+                    // DO NOTHING
+                }
+                
+            }
+
+            return null;
+        }
+
+        public string GetSoftwareProcessNameByFriendlyNamePS(string softwareName)
+        {
+
+            string processName = null;
+
+            string psScript = $@"
+                                
+                $friendlyName = '{softwareName}'
+
+                $processes = Get-Process
+
+                foreach ($process in $processes) {{
+
+                    if ($process.MainModule.FileVersionInfo.FileDescription -eq $friendlyName) {{
+
+                        Write-Output $process.ProcessName
+                        return
+                    }}
+                }}
+
+                Write-Output ''
+            ";
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-Command \"{psScript}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(startInfo))
+            {
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                processName = output;
+            }
+
+            switch (processName)
+            {
+
+                case "": return null;
+
+                default: return processName;
+            }
+        }
+
+    }
+}
